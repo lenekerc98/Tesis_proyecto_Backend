783d00da56a93399da582796ebcdcf850ba9685a
Leneker Neil Cedeño Zambrano <74921300+lenekerc98@users.noreply.github.com>
2026-03-02 22:50:53 -0500

    Refactor inferencias.py for improved structure

--- app/routers/inferencias.py
+++ app/routers/inferencias.py
@@ -197,7 +197,7 @@ def listar_inferencias(
             "url_imagen": obtener_imagen_ave(db, i.prediccion_especie),
             "latitud": i.meta_audio.latitud if i.meta_audio else None,
             "longitud": i.meta_audio.longitud if i.meta_audio else None,
-            "especie_usuario": i.especie_usuario
+            "especie_usuario": i.especie_usuario,
             "top_5": i.top_5
         }
         for i in inferencias
@@ -283,4 +283,5 @@ def agregar_especie_usuario(
     db.commit()
 
     return {"mensaje": "Especie de usuario añadida a la inferencia."}
+
 
