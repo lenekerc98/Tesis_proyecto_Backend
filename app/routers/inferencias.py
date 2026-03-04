d24785b1aac331390985d35601dbc9efc7d2124c
Leneker Neil Cedeño Zambrano <74921300+lenekerc98@users.noreply.github.com>
2026-03-03 22:09:03 -0500

    Update inferencias.py

--- app/routers/inferencias.py
+++ app/routers/inferencias.py
@@ -105,7 +105,6 @@ async def upload_audio(
     # Registrar en DB
     registrar_inferencia(
        db=db,
-       url_grabacion=None,  # Se actualizará después de subir a S3
        id_usuario=usuario.id_usuario,
        prediccion_especie=prediccion_principal,
        confianza=confianza,
@@ -337,6 +336,7 @@ def guardar_grabacion_s3(
             id_usuario=db.query(EjecucionInferencia).filter(EjecucionInferencia.log_id == log_id).first().id_usuario if db.query(EjecucionInferencia).filter(EjecucionInferencia.log_id == log_id).first() else None
         )
         raise HTTPException(status_code=500, detail="Error al guardar la grabación, intente de nuevo más tarde.")
+
 
 
 
